c63fa0f55d7eca0286e6191e7e9c5240cbd4e083
justin725817 <justinlee3324@gmail.com>
2024-04-27 18:40:44 +0800

    update first

---
+++ 課堂講義練習/CH03/CH03_0102/Account.h
@@ -0,0 +1,17 @@
+#include <string>
+
+class Account
+{
+public:
+    void setName(std::string accountName)
+    {
+        name = accountName;
+    }
+    std::string getName() const
+    {
+        return name;
+    }
+
+private:
+    std::string name;
+};
